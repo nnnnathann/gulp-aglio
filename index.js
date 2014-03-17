50844440bdc2e3d81d99a0361d72e2b3464f1426
Nathan Bleigh <nathan.bleigh@gmail.com>
2014-03-17 18:19:03 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+var aglio = require('aglio');
+var through2 = require('through2');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+var defaults = require('lodash.defaults');
+
+module.exports = function (options) {
+  'use strict';
+  // Mixes in default options.
+  options = defaults(options || {}, {
+    compress: false,
+    paths: []
+  });
+
+  function transform(file, enc, next) {
+    var self = this;
+
+    if (file.isNull()) {
+      self.push(file); // pass along
+      return next();
+    }
+
+    if (file.isStream()) {
+      self.emit('error', new PluginError('gulp-aglio', 'Streaming not supported'));
+      return next();
+    }
+
+    var str = file.contents.toString('utf8');
+
+    // Clones the options object.
+    var opts = defaults({
+      theme: 'default'
+    }, options);
+
+    // Injects the path of the current file.
+    opts.filename = file.path;
+
+    aglio.render(str, opts.theme, function (err, html) {
+      if (err) {
+        self.emit('error', new PluginError('gulp-aglio', err));
+      } else {
+        file.contents = new Buffer(html);
+        file.path = gutil.replaceExtension(file.path, '.html');
+        self.push(file);
+      }
+      next();
+    });
+  }
+
+  return through2.obj(transform);
+};
---
+++ test/test.js
@@ -0,0 +1,68 @@
+/*global it, describe */
+'use strict';
+
+var should = require('should');
+var aglio = require('../');
+var gutil = require('gulp-util');
+var fs = require('fs');
+var pj = require('path').join;
+
+function createVinyl(fixtureFileName, contents) {
+  var base = pj(__dirname, 'fixtures');
+  var filePath = pj(base, fixtureFileName);
+
+  return new gutil.File({
+    cwd: __dirname,
+    base: base,
+    path: filePath,
+    contents: contents || fs.readFileSync(filePath)
+  });
+}
+
+describe('gulp-aglio', function () {
+  describe('aglio()', function () {
+    it('should pass file when it isNull()', function (done) {
+      var stream = aglio();
+      var emptyFile = {
+        isNull: function () { return true; }
+      };
+      stream.on('data', function (data) {
+        data.should.equal(emptyFile);
+        done();
+      });
+      stream.write(emptyFile);
+    });
+
+    it('should emit error when file isStream()', function (done) {
+      var stream = aglio();
+      var streamFile = {
+        isNull: function () { return false; },
+        isStream: function () { return true; }
+      };
+      stream.on('error', function (err) {
+        err.message.should.equal('Streaming not supported');
+        done();
+      });
+      stream.write(streamFile);
+    });
+
+    it('should compile with default theme', function (done) {
+      var input = createVinyl('sample.md');
+
+      var stream = aglio({ template: 'default' });
+      stream.on('data', function (srcFile) {
+        should.exist(srcFile);
+        should.exist(srcFile.path);
+        should.exist(srcFile.relative);
+        should.exist(srcFile.contents);
+        srcFile.path.should.equal(pj(__dirname, 'fixtures', 'sample.html'));
+        String(srcFile.contents).should.containEql(
+          fs.readFileSync(pj(__dirname, 'expect/sample-default.html'),
+          'utf8')
+        );
+        done();
+      });
+      stream.write(input);
+    });
+  });
+});
